37087cc2df822782b652ea4703ac0b6ce98e7b09
Unknown <randimadissanayake.rkd@gmail.com>
2017-12-01 17:00:31 +0530

    profile

--- application/views/donor/donor-profile.php
+++ application/views/donor/donor-profile.php
@@ -7,119 +7,80 @@
    
 <body>
 
-<!-- <center>
- <input type="button" id="show_login" value="Show Login">
- <div id = "loginform">
-  <form method = "post" action = "">
-  	<div class="coverpadx"></div>
-   <p>Join TalkersCode And get Quick Access To Our Tutorials,Questions,Web Tricks And Many More</p>
-   <input type = "image" id = "close_login" src = "images/close.png">
-   <input type = "text" id = "login" placeholder = "Email Id" name = "uid">
-   <input type = "password" id = "password" name = "upass" placeholder = "***">
-   <input type = "submit" id = "dologin" value = "Login">
-  </form>
- </div>
-</center> -->
-	<div class="container">
-    <h1>User Profile</h1>
-  	<hr>
-	<div class="row">
-      <!-- left column -->
-      <div class="col-md-3">
-        <div class="text-center">
-          <img src="//placehold.it/100" class="avatar img-circle" alt="avatar">
-          <!-- <h6>Upload a different photo...</h6>
-          
-          <input type="file" class="form-control"> -->
-        </div>
-      </div>
-      
-      <!-- edit form column -->
-      <div class="col-md-9">
-        <!-- <div class="alert alert-info alert-dismissable">
-          <a class="panel-close close" data-dismiss="alert">×</a> 
-          <i class="fa fa-coffee"></i>
-          This is an <strong>.alert</strong>. Use this to show important messages to the user.
-        </div> -->
-        <h3>Randima Dissanayake</h3>
-        
-        <form class="form-horizontal" role="form">
-        	
-          <div class="coverpad"></div>
-          <div class="coverpadx">
-          	<center>
-          	<form class="form-horizontal" role="form">
-          
-          
-          <div class="form-group">
-            <label for="exampleInputEmail1">NIC Number</label>
-            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="NIC Number" name="donorNIC">
-        </div>
-        <div class="form-group">
-            <label for="exampleInputEmail1">Gender</label>
-            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Gender" name="donorGender">
-        </div>
-        <div class="form-group">
-            <label for="exampleInputEmail1">Address</label>
-            <input type="address" class="form-control" id="exampleInputEmail1" placeholder="Address" name="donorAddress">
-        </div>
-        <div class="form-group">
-            <label for="exampleInputEmail1">Occupation</label>
-            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Occupation" name="donorOccupation">
-        </div>
-        <div class="form-group">
-            <label for="exampleInputEmail1">Email Address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email Address" name="donorEmail">
-        </div>
-        <div class="form-group">
-            <label for="exampleInputEmail1">Date Of Birth</label>
-            <input type="date" class="form-control" id="exampleInputEmail1" placeholder="Date Of Birth" name="donorDOB">
-        </div>
-        <div class="form-group">
-            <label for="exampleInputEmail1">Height</label>
-            <input type="double" class="form-control" id="exampleInputEmail1" placeholder="Height" name="donorHeight">
-        </div>
+<div class="container">
+
+<ul>
+  <li>
+    <div class="row equipo-item">
+    <div class="col-md-3 separador-vertical"><img src="//placehold.it/500x500" class="img-responsive center-block" style="width: 100px; height: 100px;"></div> 
+    <div class="col-md-9">
+    <h2 class="titulo-equipo">Sumudu Hansani</h2>
+    <p class="texto-equipo">
+      rhfghdfhg
+    </p>  
+    </div>
+    </div>
+  </li>
+  <li>
+    <div class="row equipo-item">
+    <div class="col-md-3 separador-vertical"><img src="//placehold.it/500x500" class="img-responsive center-block" style="width: 100px; height: 100px;"></div> 
+    <div class="col-md-9">
+    <h2 class="titulo-equipo">Wathsala Chathurani</h2>
+    <p class="texto-equipo">
+      gfhgbjhbjhb
+    </p>  
+    </div>
+    </div>
+  </li>
+</ul>
 
 
 
-        <div class="form-group">
-            <label>Available Times</label>
-            <input type="radio" class="availableTimes" value="weekday" name="donorAvailable"><label for="weekday" class="light">Week day</label><br>
-            <input type="radio" class="availableTimes" value="weekend" name="donorAvailable"><label for="weekend" class="light">week end</label><br>
-            <input type="radio" class="availableTimes" value="anyday" name="donorAvailable"><label for="anyday" class="light">Any day</label>
+  
+  
 
-        </div>
-          </div>
-
-        </form>
-        </center>
-          </div>
-
-        </form>
-        
-      </div>
-  </div>
 </div>
-<hr>
-</body>
 <style type="text/css">
-	/*.coverpad{
-		left: 100px;
-		top: 60px;
-		background: white;
-		height: 360px;
-		width: 850px;
-		position: absolute;
-		display: none;
-	}*/
-	.coverpadx{
-		left: 50px;
-		top: 120px;
-		background: white;
-		height: 700px;
-		width: 750px;
-		position: absolute;
-		-webkit-box-shadow: 0 3px 8px rgba(0,0,0,.25);
-	}
+  .container{
+    color: white;
+  }
+  .container ul li{
+    border: 1px solid white ;
+    padding: 10px;
+    border-radius:10px;
+    margin-bottom:25px;
+  }
+  .container ul li:hover{
+    border: 1px solid red ;
+    padding: 10px;
+    border-radius:10px;
+    margin-bottom:25px;
+    background-color: green;
+    transition: linear all 0.40s;
+  }
+  .separador-vertical{
+  border-right:1px solid #bfbdbd;
+  width: 100px;
+  height: 100px;
+  }
+  
+  .titulo-equipo{
+  margin-top:0px;
+  color: black;
+      
+  }
+  .texto-equipo{
+    color: black;
+  }
+    
+  /*.equipo-item{
+    border: 1px solid red ;
+    padding: 10px;
+    border-radius:10px;
+    margin-bottom:25px;
+      }*/
+
 </style>
+
+
 </html>
