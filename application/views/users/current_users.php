0ab157cbcea4556ac3adcbb4bad3f42ff79b4a30
Sumudu Nissanka <sumudunissanke@gmail.com>
2017-12-06 12:55:19 +0530

    current user front end

--- application/views/users/current_users.php
+++ application/views/users/current_users.php
@@ -1,28 +1,70 @@
-
-<h2> <?= $title ?> </h2>
-<?php foreach ($users as $user) : ?>
-<div class="row">
-    <div class="col-md-3">
-        <?php echo $user['employee_firstname'] ?>  <?php echo $user['employee_lastname'] ?>
-    </div>
-
-    <div class="col-md-3">
-        <?php echo $user['lemployeelast_login'] ?>
-    </div>
-    <div class="col-md-2">
-        <?php echo $user['employee_occupation'] ?>
-    </div>
-    <div class="col-md-2">
-        <a  class="btn btn-danger btn-sm pull left" data-toggle="modal" data-target="#block">Block</a>
-    </div>
-    <div class="col-md-2">
-        <a class="btn btn-primary btn-sm " href="<?php echo site_url('/users/'.$user['employee_id'])?>">Edit</a>
-    </div>
-    <br>
+<style>
+    .thead th{
+        background-color: #000777;
+        color: white;
+        text-align: center;
+    }
+    .id{
+        background-color: #000688;
+        color: white;
+    }
+    .b{
+        padding: 5px;
+    }
+    .none a{
+        text-decoration: none;
+    }
+    .detail{
+        margin-top: 0px;
+    }
+</style>
+<div class="col-md 12" style="text-align: center;">
+    <h2> <?= $title ?> </h2>
 </div>
-    <hr>
-<?php endforeach;?>
 
+    <div class="row detail" style="text-align: center;margin-left: 140px; ">
+    <div class="col-md-10 offset-1">
+        <table class="table table-hover table-responsive table-striped table-bordered">
+            <thead style="background-color: #365D67;">
+            <tr>
+                <th >
+                    Employee Name
+                </th>
+
+                <th>
+                    Last login time
+                </th>
+                <th>
+                    Occupation
+                </th>
+                <th>
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($users as $user) : ?>
+            <tr>
+                <td>
+                    <?php echo $user['employee_firstname'] ?>  <?php echo $user['employee_lastname'] ?>
+                </td>
+
+                <td>
+                    <?php echo $user['lemployeelast_login'] ?>
+                </td>
+                <td>
+                    <?php echo $user['employee_occupation'] ?>
+                </td>
+                <td>
+                    <a  class="btn btn-danger btn-sm pull left" data-toggle="modal" data-target="#block" style="border:thin; padding-left: 5px;"><span><i class="fa fa-lock"aria-hidden="true"></i></span>Block</a>
+                    <a class="btn btn-primary btn-sm " href="<?php echo site_url('/users/'.$user['employee_id'])?>"><span><i class="fa fa-pencil" ></span></i>Edit</a>
+                </td>
+            </tr>
+            <?php endforeach;?>
+            </tbody>
+        </table>
+    </div>
+</div>
+</table>
 <div class="pagination-links">
     <?php echo $this->pagination->create_links();?>
 </div>
